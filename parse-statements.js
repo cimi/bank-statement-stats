57db42e5fecff38ca064fa84973f57bd61ea4ba5
cimi <alex.ciminian@gmail.com>
2014-07-21 00:15:31 +0100

    Parse payment date with momentjs

--- parse-statements.js
+++ parse-statements.js
@@ -1,5 +1,6 @@
 'use strict';
 var $ = require('cheerio')
+  , moment = require('moment')
   , fs = require('fs');
 
 var dir = './statements';
@@ -11,6 +12,14 @@ function extractTable(content) {
     return $(this).attr('summary') === summary;
   });
 }
+// payment:
+//  0 => date
+//  1 => type
+//  2 => name
+//  3 => outgoing amount
+//  4 => incoming amount
+//  5 => balance
+//  6 => debit?
 
 function extractPayments(table) {
   var rows = table.find('tbody tr')
@@ -26,13 +35,29 @@ function extractPayments(table) {
   return allPayments;
 }
 
+// TODO: payment type in human readable form
+// TFR => transfer
+// VIS => card payment
+// CR => credit (incoming payment)
+// ((( => contactless payment
+
+function processPaymentDate(year) {
+  return function (payment) {
+    payment[0] = moment(new Date(year + ' ' + payment[0])).format('YYYY-MM-DD');
+    return payment;
+  };
+}
+
 fs.readdir(dir, function (err, files) {
-  if (err) throw err;
   files.forEach(function (file) {
     var filePath = dir + '/' + file
       , outputDir = './app/data'
-      , table = extractTable(fs.readFileSync(filePath).toString());
-      fs.writeFileSync(outputDir + '/' + file,
-        JSON.stringify(extractPayments(table), null, "  "));
+      , table = extractTable(fs.readFileSync(filePath).toString())
+      , year = file.split('-')[0];
+
+    var payments = extractPayments(table).map(processPaymentDate(year));
+
+    fs.writeFileSync(outputDir + '/' + file,
+      JSON.stringify(payments, null, '  '));
   });
 });
